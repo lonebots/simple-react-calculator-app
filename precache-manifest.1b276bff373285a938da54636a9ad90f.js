444cc96f136338ab4bd0fb6057363d9a0b85681a
lonebots <jishnusurajila@gmail.com>
2022-11-15 22:09:49 +0530

    Updates

---
+++ precache-manifest.1b276bff373285a938da54636a9ad90f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "34f9b4838ede41db47bc",
+    "url": "/simple-react-calculator-app/static/css/main.67e920ad.chunk.css"
+  },
+  {
+    "revision": "34f9b4838ede41db47bc",
+    "url": "/simple-react-calculator-app/static/js/main.de1d549f.chunk.js"
+  },
+  {
+    "revision": "0b54f72dd63ed30e2847",
+    "url": "/simple-react-calculator-app/static/js/runtime~main.2212e95b.js"
+  },
+  {
+    "revision": "15fdcd5c0b129c1bb8d4",
+    "url": "/simple-react-calculator-app/static/js/2.b6c8d9b3.chunk.js"
+  },
+  {
+    "revision": "eedfb1b54cc50ef37ed5",
+    "url": "/simple-react-calculator-app/static/js/3.e8d7c53c.chunk.js"
+  },
+  {
+    "revision": "c43f59d772c0e1e61f3cde39a28ff485",
+    "url": "/simple-react-calculator-app/index.html"
+  }
+];
